fb8212b14127d53748c212da7f12b59908ec2219
Searching4Myeslf <88655156+AreexYT@users.noreply.github.com>
2022-12-04 03:30:14 -0600

    Update Anti-Display-Name-Loader-v7.lua

--- UI/Leaderboard/Anti-Display-Name-Loader-v7.lua
+++ UI/Leaderboard/Anti-Display-Name-Loader-v7.lua
@@ -4,6 +4,12 @@ Anti Diplay Name v5.lua
 
 ]]--
 
+if not game['Loaded'] or not game:GetService('Players')['LocalPlayer'] then
+    game['Loaded']:Wait();
+    game:WaitForChild(game:GetService('Players'));
+    game:GetService('Players'):WaitForChild(game:GetService('Players').LocalPlayer.Name)
+end
+
 while wait(0.5) do
 	
 	local AntiDisplayName = loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/Roblox-Scripts/main/UI/Leaderboard/Anti-Display-Name-v7.lua'))(function()
